b3887d9e3bdc43c20e4c64bd2b871b550db80b00
Abhiram Raghunand <abhiram261104@gmail.com>
2026-02-15 13:05:38 +0530

    Fix form error

--- application.py
+++ application.py
@@ -28,6 +28,7 @@ def predict_data():
         BUI=float(request.form.get("BUI"))
         Classes=float(request.form.get("Classes"))
         Region=float(request.form.get("Region"))
+        print(request.form)
 
         scaled_data=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,BUI,Classes,Region]])
         result=ridge_model.predict(scaled_data)
